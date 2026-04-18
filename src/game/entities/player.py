3f0ad053d475ca1170afdc04a3c82a0b70e6e0f2
Luke Fabrin <lukefabrin@gmail.com>
2026-04-18 13:35:20 +1200

    edited movement, no verticla movement just a d and also space to launch.

--- src/game/entities/player.py
+++ src/game/entities/player.py
@@ -1,19 +1,26 @@
 import pygame
-import sys
 
+class Player:
+    def __init__(self, x, y, size=40, speed=5, color=(0, 200, 255)):
+        self.x = x
+        self.y = y
+        self.size = size
+        self.speed = speed
+        self.color = color
 
+    def handle_input(self):
+        keys = pygame.key.get_pressed()
 
-# Key input (continuous)
-keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE]:
+            self.launch()
+        if keys[pygame.K_a]:
+            self.x -= self.speed
+        if keys[pygame.K_d]:
+            self.x += self.speed
 
-if keys[pygame.K_w]:
-    player_y -= player_speed
-if keys[pygame.K_s]:
-    player_y += player_speed
-if keys[pygame.K_a]:
-    player_x -= player_speed
-if keys[pygame.K_d]:
-    player_x += player_speed/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-
-pygame.draw.rect(screen, (0, 200, 255), (player_x, player_y, player_size, player_size))
+    def draw(self, screen):
+        pygame.draw.rect(
+            screen,
+            self.color,
+            (self.x, self.y, self.size, self.size)
+        )
